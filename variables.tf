de49aa6f7bca7e767559129f5d857858ff12bb5a
koothemaverick <junhui9789@gmail.com>
2025-08-29 13:45:13 +0900

    terraform, Dockerfile, 응답 format 수정

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "DM-LangGraph"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "v"
+}
